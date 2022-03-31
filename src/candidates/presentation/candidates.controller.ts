09b86afdc92ce95ce6b419684a88f7d0a3d8bfd9
Florian Lafuente <florian.lafuente@gmail.com>
2022-03-31 16:29:14 +0200

    fix swagger

--- src/candidates/presentation/candidates.controller.ts
+++ src/candidates/presentation/candidates.controller.ts
@@ -1,5 +1,5 @@
 import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
-import {ApiResponse, ApiTags } from '@nestjs/swagger';
+import {ApiResponse, ApiTags} from '@nestjs/swagger';
 import {CandidatesService} from '../application/candidates.service';
 import { CandidateDto } from './dto/candidate.dto';
 import {CreateCandidateDto} from './dto/create-candidate.dto';
@@ -12,8 +12,8 @@ export class CandidatesController {
     }
 
     @Post()
-    @ApiResponse({ status: 200, description: 'Create one candidate.'})
-        create(@Body() createCandidateDto: CreateCandidateDto) : CandidateDto{
+    @ApiResponse({ status: 201, description: 'Create one candidate.', type: CandidateDto})
+    create(@Body() createCandidateDto: CreateCandidateDto): CandidateDto {
         const candidate = new CandidateDto();
         candidate.picture = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
         candidate.firstname = 'Sam';
@@ -26,14 +26,22 @@ export class CandidatesController {
     }
 
     @Get()
-    @ApiResponse({ status: 200, description: 'Get all candidates.'})
-    findAll() {
-        return this.candidatesService.findAll();
+    @ApiResponse({ status: 200, description: 'Get all candidates.', type: CandidateDto, isArray: true})
+    findAll(): CandidateDto[] {
+        const candidate = new CandidateDto();
+        candidate.picture = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
+        candidate.firstname = 'John';
+        candidate.lastname = 'DOE';
+        candidate.mail = 'johndoe@florian.lafuente.fr';
+        candidate.keywords = ['php', 'javascript', 'html', 'css'];
+        candidate.salary = 2000;
+        candidate.offerType = ['cdi', 'cdd'];
+        return [candidate]
     }
 
     @Get(':id')
-    @ApiResponse({ status: 200, description: 'Get one candidate.'})
-    findOne(@Param('id') id: string) : CandidateDto{
+    @ApiResponse({ status: 200, description: 'Get one candidate.', type: CandidateDto})
+    findOne(@Param('id') id: string): CandidateDto {
         const candidate = new CandidateDto();
         candidate.picture = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
         candidate.firstname = 'John';
@@ -46,8 +54,8 @@ export class CandidatesController {
     }
 
     @Patch(':id')
-    @ApiResponse({ status: 200, description: 'Edit a candidate.'})
-    update(@Param('id') id: string, @Body() updateCandidateDto: UpdateCandidateDto) : CandidateDto{
+    @ApiResponse({ status: 200, description: 'Edit a candidate.', type: CandidateDto})
+    update(@Param('id') id: string, @Body() updateCandidateDto: UpdateCandidateDto): CandidateDto {
         const candidate = new CandidateDto();
         candidate.picture = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
         candidate.firstname = 'John';
@@ -62,6 +70,6 @@ export class CandidatesController {
     @Delete(':id')
     @ApiResponse({ status: 200, description: 'Delete a candidate.'})
     remove(@Param('id') id: string) {
-        return this.candidatesService.remove(+id);
+        return ;
     }
 }
