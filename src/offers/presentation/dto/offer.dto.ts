4e139bec1a991cbddf4764ce58be42890d059211
Benoit <43681702+BenoitYnov@users.noreply.github.com>
2022-03-31 17:28:47 +0200

    init Modules offers (#11)
    * Init Module Offers
    
    * Correction module offers
    
    * Mock methode Create
    
    Co-authored-by: Benoit Thrive <benoit@agencethrive.fr>

--- src/offers/presentation/dto/offer.dto.ts
+++ src/offers/presentation/dto/offer.dto.ts
@@ -1,2 +1,16 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class OfferDto {
+    @ApiProperty()
+    description: string
+    @ApiProperty()
+    keywords: string[]
+    @ApiProperty()
+    salary: number
+    @ApiProperty()
+    type: string[]
+    @ApiProperty()
+    status: string
+    @ApiProperty()
+    isOpen: boolean
 }
