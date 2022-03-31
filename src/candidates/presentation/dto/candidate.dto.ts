ce88c19f519b2182f4746d53288328458f1284da
Soussou <soulei.seg@gmail.com>
2022-03-31 15:19:29 +0200

    Added api properties to CandidateDto + extended CreateCandidateDto to CandidateDto

--- src/candidates/presentation/dto/candidate.dto.ts
+++ src/candidates/presentation/dto/candidate.dto.ts
@@ -1,2 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CandidateDto {
+    @ApiProperty()
+    picture: string
+    @ApiProperty()
+    firstname: string
+    @ApiProperty()
+    lastname: string
+    @ApiProperty()
+    mail: string
+    @ApiProperty()
+    keywords: []
+    @ApiProperty()
+    salary: number
+    @ApiProperty()
+    offerType: number
 }
