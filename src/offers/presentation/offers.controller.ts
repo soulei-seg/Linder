d175a7857bbabf3da417874fe7cafbe3e2e2b4c3
Benoit Thrive <benoit@agencethrive.fr>
2022-03-31 17:17:50 +0200

    Init Module Offers

--- src/offers/presentation/offers.controller.ts
+++ src/offers/presentation/offers.controller.ts
@@ -1,8 +1,9 @@
 import {Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {ApiResponse, ApiTags} from '@nestjs/swagger';
 import {OffersService} from '../application/offers.service';
 import {CreateOfferDto} from './dto/create-offer.dto';
 import {UpdateOfferDto} from './dto/update-offer.dto';
+import {OfferDto} from "./dto/offer.dto";
 
 @Controller('offers')
 @ApiTags('offers')
@@ -10,26 +11,49 @@ export class OffersController {
     constructor(private readonly offersService: OffersService) {
     }
 
+    @ApiResponse({status: 201, description: 'Create a new offer'})
     @Post()
     create(@Body() createOfferDto: CreateOfferDto) {
         return this.offersService.create(createOfferDto);
     }
 
+    @ApiResponse({status: 200, type: OfferDto, isArray: true, description: 'Get all offers'})
     @Get()
-    findAll() {
-        return this.offersService.findAll();
+    findAll(): OfferDto[] {
+        const offer = new OfferDto()
+        offer.description = "Nouvelle offre"
+        offer.keywords = ['PHP', 'html']
+        offer.status = "is Open"
+        offer.salary = 24000
+        offer.isOpen = false
+        return [offer]
     }
 
+    @ApiResponse({status: 200, type: OfferDto, description: 'Get one offer'})
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.offersService.findOne(+id);
+    findOne(@Param('id') id: string): OfferDto {
+        const offer = new OfferDto()
+        offer.description = "Nouvelle offre"
+        offer.keywords = ['PHP', 'html']
+        offer.status = "is Open"
+        offer.salary = 24000
+        offer.isOpen = false
+        return offer
     }
 
+    @ApiResponse({status: 200, type: OfferDto, description: 'Edit an offer'})
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
-        return this.offersService.update(+id, updateOfferDto);
+    update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto): OfferDto {
+        const offer = new OfferDto()
+        offer.description = "Nouvelle offre interressante"
+        offer.keywords = ['PHP', 'html']
+        offer.status = "is Open"
+        offer.salary = 24000
+        offer.isOpen = false
+        return offer
     }
 
+    @ApiResponse({status: 200, description: 'Delete an offer'})
     @Delete(':id')
     remove(@Param('id') id: string) {
         return this.offersService.remove(+id);
