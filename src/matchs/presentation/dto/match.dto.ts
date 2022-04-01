a050e4b1a65f2e85420faeb2d5d9b295740928f3
antoinenicolleau <antoine.nicolleau@ynov.com>
2022-04-01 10:14:33 +0200

    Mock Matches

--- src/matchs/presentation/dto/match.dto.ts
+++ src/matchs/presentation/dto/match.dto.ts
@@ -1,2 +1,14 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {OfferDto} from "../../../offers/presentation/dto/offer.dto";
+import {ProfileDto} from "../../../profiles/presentation/dto/profile.dto";
+
 export class MatchDto {
+    @ApiProperty()
+    offer: OfferDto;
+
+    @ApiProperty()
+    profile: ProfileDto;
+
+    @ApiProperty()
+    status: string;
 }
